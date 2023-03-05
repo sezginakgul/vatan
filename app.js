8c3561bb18df3405c6c691388cfbd0d6521f5a21
sezgin <sezgin.akgull@gmail.com>
2023-03-05 22:09:04 +0300

    update

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ let hi = document.querySelector(".hi");
 // let cargo = document.querySelectorAll(".cargo");
 let change = document.querySelector(".change");
 let cargo = document.querySelector(".cargo");
-let sepet = document.querySelectorAll(".ekle");
+// let sepet = document.querySelectorAll(".ekle");
 let cardParent = document.querySelector(".card-parent");
 
-let a = [1, 2, 3, 4, 5, 6];
+let a = [1, 2, 3];
 
 a.map((i) => {
   const newChange = document.createElement("div");
@@ -26,13 +26,14 @@ a.map((i) => {
     <div style="font-size: 11px">PA2N0006TR</div>
     <h6 class="card-title">iPhone 11 64 Gb Akıllı Telefon Beyaz</h6>
 
-    <div class="card-text fs-4 fw-bold text-primary">₺ 15.999</div>
+    <div class="card-text fs-4 fw-bold text-primary price">₺ 15.999</div>
     </div>`;
   cardParent.appendChild(newChange);
 });
 
 let card = document.querySelectorAll(".card");
 const newDiv = document.createElement("div");
+newDiv.classList.add("change");
 newDiv.innerHTML = `<div class="text-center p-2">
       <div class="my-2">
       <span
@@ -58,12 +59,12 @@ style="font-size: 12px"
   i.addEventListener("mouseenter", (e) => {
     i.style.border = "1px solid blue";
     // if (!newDiv.innerHTML.length) {
-    newDiv.innerHTML = `<div type="button" class="d-flex mx-auto bg-primary text-light ">
+    newDiv.innerHTML = `<div type="button" class="end d-flex mx-auto bg-primary text-light ">
       <div class="bg-info p-2">
       <i class="fa-solid fa-arrow-right-arrow-left"></i>
       </div>
-      <div class="sepet d-flex align-items-center justify-content-center flex-fill ">
-      <div>SEPETE EKLE</div>
+      <div class=" d-flex align-items-center justify-content-center flex-fill ">
+      <div class="sepet">SEPETE EKLE</div>
       </div>
       </div>  `;
     // }
@@ -85,3 +86,51 @@ style="font-size: 12px"
     i.appendChild(newDiv);
   });
 });
+const end = document.querySelectorAll(".end");
+end.forEach((e) => {
+  i.addEventListener("mouseleave", () => {
+    i.style.border = "";
+
+    i.lastElementChild.innerHTML = `<div class="text-center p-1">
+    <div class="mb-2">
+    <span
+class="cargo bg-body-secondary rounded-1 p-2 shadow-sm"
+style="font-size: 12px"
+>BUGÜN KARGODA</span
+>
+</div></div> `;
+
+    i.appendChild(newDiv);
+  });
+});
+// const price = document.querySelectorAll(".price");
+// const sepet = document.querySelector(".sepet");
+// card.forEach((p) => {
+// console.log(p);
+cardParent.addEventListener("click", (e) => {
+  console.log(e.target.classList);
+  if (e.target.classList.contains("sepet")) {
+    sepetCount += 1;
+    console.log("sepetCount", sepetCount);
+    newNumber.innerHTML = `
+<span
+                  class="position-absolute top-0 md-start-100 translate-middle badge rounded-pill bg-danger"
+                >
+                  ${sepetCount}
+                </span>
+`;
+  }
+});
+// });
+
+let cartNumber = document.querySelector(".cart-number");
+const newNumber = document.createElement("span");
+let sepetCount = 0;
+newNumber.innerHTML = `
+<span
+                  class="position-absolute top-0 md-start-100 translate-middle badge rounded-pill bg-danger"
+                >
+                  ${sepetCount}
+                </span>
+`;
+cartNumber.appendChild(newNumber);
